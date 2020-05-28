12b88f60b0d3ec12fb07f1bf2b6071440dff8ccd
wizardoma <alibekason@gmail.com>
2020-05-28 07:34:30 +0100

    First Commit on project setup

---
+++ src/main/java/com/techbek/steibsfloral/SteibsfloralApplication.java
@@ -0,0 +1,13 @@
+package com.techbek.steibsfloral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteibsfloralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteibsfloralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techbek/steibsfloral/SteibsfloralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbek.steibsfloral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteibsfloralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
